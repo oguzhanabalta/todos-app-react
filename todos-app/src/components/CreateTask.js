82bda7e8b15b244d22de58090fdde85ba186c61e
Oğuzhan BALTA <45859029+oguzhanabalta@users.noreply.github.com>
2021-09-13 14:01:50 +0300

    Created Components

---
+++ todos-app/src/components/CreateTask.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateTask extends Component {
+    render() {
+        return (
+            <div>
+                CreateTask Here!
+            </div>
+        )
+    }
+}
---
+++ todos-app/src/components/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CreateTask from "./CreateTask";
+import TaskList from "./TaskList";
+
+export default class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.sate = {
+            tasks: [],
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h1>Todos</h1>
+                <div>
+                    <CreateTask />
+                    <br/>
+                    <TaskList />
+                </div>
+            </div>
+        )
+    }
+}
